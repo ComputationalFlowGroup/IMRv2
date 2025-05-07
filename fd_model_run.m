eb3db4a0dfa4293592e20ba6eb98b41dd4df7c75
mcarcana <mirelys_carcana_barbosa@brown.edu>
2025-05-07 16:48:29 -0400

    need this to run stress contour graphs

--- fd_model_run.m
+++ fd_model_run.m
@@ -12,13 +12,15 @@ kappa = 1.4;
 Lheat = 2.378193575129533e+04;
 T8 = 298.15;
 rho8 = 1064;
-tvector = linspace(0,tfin,100);
+tvector = linspace(0,tfin,500);
 radial = 1;
 vapor = 1;
-bubtherm = 1;
-medtherm = 1;
-masstrans = 1;
-stress = [9,2];
+bubtherm = 0;
+medtherm = 0;
+masstrans = 0;
+stress = [8,2];
+G = 2500;
+dGdhs = 1E+4;
 for i = 1:length(stress)
 varin = {'progdisplay',0,...
     'radial',radial,...
@@ -29,30 +31,36 @@ varin = {'progdisplay',0,...
          'method',23,...
          'stress',stress(i),...
          'collapse',1,...
-         'mu',1E-4,...
-         'g',2500,...
+         'mu',1E-3,...
+         'g',G,...
          'lambda1',1e-7,...
          'lambda2',0,...
-         'alphax',1e-3,...
-         'dgdhs',1,...
+         'alphax',1,...
+         'dgdr0',dGdr0,...
          'r0',R0,...
          'req',Req,...
          'kappa',kappa,...
          't8',T8,...
          'rho8',rho8};
-
-[t1,R1,~] = m_imr_fd(varin{:},'Nt',70,'Mt',70);
-%[t1,R1,~] = m_imr_fd(varin{:},'Nt',50,'Mt',300);
+Pref = 101325;
+Ca = Pref/G;
+%[t,R,~] = m_imr_fd(varin{:},'Nt',70,'Mt',70);
+[t,R,~] = m_imr_fd(varin{:},'Nt',50,'Mt',300); %mu = 1E-4, alphax = 1e-3
+G = 2500; dGdhs = 1E+4; 
 
 figure(1)
 hold on;
-plot(t1,R1,'x')
+plot(t,R,'x')
 ylim([0 1.2])
-legend('nHqKV-gmlin-fd','nHqKV-fd')
+legend('nHKV-gmlin-fd','nHKV-fd')
+Lambda = R0/Req;
+r_ffield_baseline
 
-figure(1)
-hold on;
-[t2,R2,~] = m_imr_spectral(varin{:},'Nt',12,'Mt',12);
-plot(t2,R2,'^')
-legend('nHqKV-gmlin-sp','nHqKV-sp')
+
+% figure(1)
+% hold on;
+% [t2,R2,~] = m_imr_spectral(varin{:},'Nt',12,'Mt',12);
+% plot(t2,R2,'^')
+% legend('nHKV-gmlin-sp','nHKV-sp')
+% %legend('nHqKV-gmlin-sp')
 end
