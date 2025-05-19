707ee4fd1737eb8b714e8044221258fa0e941659
Mauro Rodriguez <mauro_rodriguez@brown.edu>
2025-05-19 15:33:49 -0400

    small fix to the graded figure

--- examples/s_graded_example.m
+++ examples/s_graded_example.m
@@ -1,19 +1,19 @@
 clc; clear; close all;
 
 
-Rst = linspace(0.5,10,200);
+Rst = linspace(0.25,10,200);
 
-G1 = 1;
-G3 = 2;
+G1 = 0.5;
+G3 = 1;
 
-l1 = 1.2;
-l2 = 2;
-a = 1.2;
+l1 = 1.5;
+l2 = 3;
+a = 2;
 n = 0.3;
 x1 = @(Rst) (1+(Rst.^3-1)./(1+l1).^3).^(1/3);
 x2 = @(Rst) (1+(Rst.^3-1)./(1+l2).^3).^(1/3);
 
-ycy = @(x,Rst) (G3+(G1-G3)*(1+((x-x1(Rst))./(x2(Rst)-x)).^a).^((n-1)/a)).*(1./x.^5+1./x.^2);
+ycy = @(x,Rst) (1+(G1-1)*(1+((x-x1(Rst))./(x2(Rst)-x)).^a).^((n-1)/a)).*(1./x.^5+1./x.^2);
 % ype = @(x,Rst) (G3+(G1-G3)*asinh((x-x1(Rst))./(x2(Rst)-x))).*(1./x.^5+1./x.^2);
 % ympe = @(x,Rst) (G3+(G1-G3)*log((x-x1(Rst))./(x2(Rst)-x)+1)./((x-x1(Rst))./(x2(Rst)-x)).^a).*(1./x.^5+1./x.^2);
 % ycr = @(x,Rst) (G3+(G1-G3)*1./(1+(x-x1(Rst))./(x2(Rst)-x)).^n).*(1./x.^5+1./x.^2);
@@ -22,7 +22,7 @@ ycy = @(x,Rst) (G3+(G1-G3)*(1+((x-x1(Rst))./(x2(Rst)-x)).^a).^((n-1)/a)).*(1./x.
 
 reltol = 1e-8;
 abstol = 1e-8;
-S2 = zeros(length(Rst),1);
+S2 = zeros(1,length(Rst));
 for i = 1:length(Rst)
     rst = Rst(i);
     S2(i) = 2*integral(@(x) ycy(x,rst),x1(rst),x2(rst),...
@@ -42,9 +42,9 @@ hold on;
 plot(Rst,S1+S2+S3,'-.g','LineWidth',3)
 plot(Rst,SG1,'r','LineWidth',3)
 plot(Rst,SG3,'k--','LineWidth',3)
-ylim([-6 6])
+ylim([-5 5])
 xlabel('$R_{\mathrm{max}}/R_{0}$', 'Interpreter', 'Latex', 'FontSize', 20);
-ylabel('$S$','Interpreter','Latex','FontSize',24);
+ylabel('$S/G_1$','Interpreter','Latex','FontSize',24);
 set(gcf,'color','w');
 set(gca,'FontName','Times','FontSize',20);
 set(gca,'TickLabelInterpreter','latex')
@@ -53,5 +53,7 @@ xa.TickLength = [.015 .015];
 xa.LineWidth = 1.5;
 tickrange= 0:2:10;
 xticks(tickrange)
+tickrange= -5:2:5;
+yticks(tickrange)
 box on;
 saveas(gcf,'./fig_graded_stress_integral','png')
