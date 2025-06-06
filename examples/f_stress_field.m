fcb3319ff4fe2c1666052c7bee3e1210e21b0979
mcarcana <mirelys_carcana_barbosa@brown.edu>
2025-06-06 14:27:42 -0400

    stress fields almost ready, the l1 line not showing up, still needs debugging

--- examples/f_stress_field.m
+++ examples/f_stress_field.m
@@ -1,23 +1,25 @@
-fig_tend = 5;
-tickrange= 0:5:fig_tend;
+clc;
+fig_tend = 4;
+%tickrange= 0:2:fig_tend;
+tickrange = ceil(min(t)) : floor(max(t));
 lR = length(R);
-lr_max = 100;
-lr_N = 200;
+%lr_max = 100;
+lr_N = 500;
 lr_length = 1;
+nt = length(t);
 r_coord = ones(lR,lr_N).*logspace(-0.5,lr_length,lr_N);
 R_coord = (ones(lR,lr_N).*R);
-%r0_coord = (r_coord.^3 - R_coord.^3 + (Req/R_coord).^3).^(1/3);
 
 % calculating the shear
 %varsigmadot_r = f_gammadot_r(r_coord,R,U,lR,lr_N);
 [xcon,ycon] = meshgrid(t,r_coord(1,:));
 ycon = log10(ycon);
-clevels = 50;
+clevels = 1000;
 % feps_r = f_f_filter(f_r,lR,lr_N);
 % tau_r = 2*(feps_r./DRe+1./Re8).*varsigmadot_r;
 %ntau_r = tau_r/max_tau_r;
-taurr = gradedstress(lR,R_coord,Req,Ca,Ca1,l1,l2,v_nc,v_a);
-maxtaurr = max(taurr);
+[taurr,r1,r2] = gradedstress(r_coord',R_coord',Req,Ca,Ca1,l1,l2,v_nc,v_a);
+maxtaurr = 1; %max(abs(taurr(:))); %1; %max(max(abs(taurr)));
 ntaurr = taurr/maxtaurr;
 % N = length(taurr);
 % M = lr_N;
@@ -36,12 +38,13 @@ colormap jet;
 cbar = colorbar;
 %cbar.Label.String = '$m(\dot{\varsigma})$';
 cbar.Label.String = '$\tau_{rr}/p_{\infty}$';
-set(cbar,'TickLabelInterpreter','latex');
+set(cbar,'TickLabelInterpreter','latex','FontSize',18);
 pos = get(cbar,'Position');
-cbar.Label.Position = [pos(1) -0.1];
+cbar.Label.Position = [pos(1) -0.9];
 cbar.Label.Rotation = 0;
 cbar.Label.Interpreter = 'latex';
-clim([0 1]);
+%clim([min(ntaurr(:)) max(ntaurr(:))]);
+clim([-0.75 .25])
 %clim([0.1 0]);
 xlim([0 fig_tend]);
 xticks(tickrange)
@@ -51,31 +54,37 @@ set(gca,'TickLabelInterpreter','latex')
 xa = gca;
 xa.TickLength = [.015 .015];
 xa.LineWidth = 1.5;
+ytickrange = round(linspace(log10(min(r_coord(:))),log10(max(r_coord(:))),3),2);
+yticks(ytickrange)
+ya = gca;
+ya.TickLength = [.015 .015];
+ya.LineWidth = 1.5;
 box on;
 plot(t, log10(R),'LineWidth',3,'Color','k');
-contourf(xcon',ycon',ntaurr,clevels,'edgecolor','none')
+%plot(t, R,'LineWidth',3,'Color','k');
+contourf(xcon,ycon,ntaurr,clevels,'edgecolor','none')
+hold on;
+plot(t,log10(r1'),'b--','LineWidth',2,'DisplayName','l1')
+plot(t,log10(r2'),'r--','LineWidth',2,'DisplayName','l2')
 %saveas(gcf,'./figs/baseline/fcon_T','png')
+%ylim([0 1])
+ylim([log10(min(r_coord(:))), log10(max(r_coord(:)))])
 
-function [taurr] = gradedstress(lR,R_coord,Req,Ca,Ca1,l1,l2,v_nc,v_a)
-    reltol = 1e-8;
-    abstol = 1e-8;
-    Se1 = zeros(1,lR); Se2 = zeros(1,lR); Se3 = zeros(1,lR); taurr = zeros(1,lR);
-    for i = 1:lR
-        Rst = R_coord(i)/Req;
-        x1 = (1 + (Rst.^3 - 1)./(l1^3)).^(1/3);
-        x2 = (1 + (Rst.^3 - 1)./(l2^3)).^(1/3);
+function [taurr,r1,r2] = gradedstress(r_coord,R_coord,Req,Ca,Ca1,l1,l2,v_nc,v_a)
+   aa = r_coord.^3 - R_coord.^3 + Req^3;
+   aa = (1./(1-(aa<0))).*aa;
+   r0_coord = real((aa).^(1/3));
     
-        f_cy = @(x) (l2*((x.^3 - 1)/(Rst.^3 - 1)).^(1/3) - 1)/(1-l1*((x.^3 - 1)/(Rst.^3 - 1)).^(1/3));
-        g = @(x) (1/Ca + (1/Ca1 - 1/Ca)*(1+f_cy(x(i)).^v_a).^((v_nc-1)/v_a)).*((1./x.^5) + (1./x.^2));
-    
-        %Sv = - 4/Re8*Rdot/R - 6*intfnu*iDRe;
+   f_cy = (l2 - r0_coord) ./ (r0_coord - l1);
         
-        Se1(i) = (1/(2*Ca))*(1/(Rst.^4) + 4/Rst - (1./x1.^4 + 4./x1));
-        Se2(i) = 2*integral(@(x) g(x),x1,x2,'RelTol',reltol,'AbsTol',abstol);
-        Se3(i) = -(1/(2*Ca1))*(5 - 4./x2 - 1./x2.^4);
-        
-        taurr(i) = Se1(i) + Se2(i) + Se3(i);
-    end
+   taurr1 = (2/(3*Ca))*((r0_coord.^4 ./ r_coord.^4) - (r_coord.^2 ./ r0_coord.^2));
+   taurr2 = ((1/Ca) + (1/Ca1 - 1/Ca)*(1+f_cy.^v_a).^((v_nc-1)/v_a)).*((r0_coord.^4 ./ r_coord.^4) - (r_coord.^2 ./ r0_coord.^2));
+   taurr3 = (2/(3*Ca1))*((r0_coord.^4 ./ r_coord.^4) - (r_coord.^2 ./ r0_coord.^2));
+
+   taurr = taurr1 + taurr2 + taurr3;
+
+   r1 = (l1^3 + R_coord.^3 - Req^3).^(1/3);
+   r2 = (l2^3 + R_coord.^3 - Req^3).^(1/3);
 end
 
 % shear as a function of r (radial coordinate) calculation
