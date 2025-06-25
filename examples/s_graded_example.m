c812d1f9e3ba1c94a15ac5ff36b56ee1a48bab8a
mcarcana <mirelys_carcana_barbosa@brown.edu>
2025-06-25 10:03:54 -0400

    graded region stress bound between stress of nongraded regions

--- examples/s_graded_example.m
+++ examples/s_graded_example.m
@@ -1,7 +1,7 @@
 clc; clear; close all;
+%% figure 1 JMPS
 
-
-Rst = linspace(0.2,10,200);
+Rst = linspace(0.2,10,200); %R_max/R_0
 
 G1 = 0.5;
 G3 = 1;
@@ -11,8 +11,8 @@ l1 = 1.5;
 l2 = 3;
 a = 2;
 n = 0.3;
-x1 = @(Rst) (1+(Rst.^3-1)./(l1).^3).^(1/3);
-x2 = @(Rst) (1+(Rst.^3-1)./(l2).^3).^(1/3);
+x1 = @(Rst) (1+(Rst.^3-1)./(l1).^3).^(1/3); %Lambda1
+x2 = @(Rst) (1+(Rst.^3-1)./(l2).^3).^(1/3); %Lambda2
 
 % ycy = @(x,Rst) (1+(G1-1)*(1+((x-x1(Rst))./(x2(Rst)-x)).^a).^((n-1)/a)).*(1./x.^5+1./x.^2);
 ycy = @(x,Rst) (G3+(G1-G3)*(1+( (l2*((x.^3 - 1)./(Rst.^3 - 1)).^(1/3) - 1)./...
@@ -23,6 +23,7 @@ ycy = @(x,Rst) (G3+(G1-G3)*(1+( (l2*((x.^3 - 1)./(Rst.^3 - 1)).^(1/3) - 1)./...
 % yscr = @(x,Rst) (G3+(G1-G3)*1./(1+(x-x1(Rst))./(x2(Rst)-x))).*(1./x.^5+1./x.^2);
 % ymcr = @(x,Rst) (G3+(G1-G3)*(1./(1+(x-x1(Rst))./(x2(Rst)-x)).^n).^a).*(1./x.^5+1./x.^2);
 
+%%
 reltol = 1e-8;
 abstol = 1e-8;
 S2 = zeros(1,length(Rst));
@@ -42,9 +43,9 @@ hold on;
 % plot(Rst,S1,'m')
 % plot(Rst,S2,'k')
 % plot(Rst,S3,'b')
-plot(Rst,SG1,'r','LineWidth',3)
-plot(Rst,SG3,'k--','LineWidth',3)
-plot(Rst,S1+S2+S3,'-.g','LineWidth',3)
+plot(Rst,SG1/G1,'r','LineWidth',3)
+plot(Rst,SG3/G1,'k--','LineWidth',3)
+plot(Rst,(S1+S2+S3)/G1,'-.g','LineWidth',3)
 ylim([-5 5])
 xlabel('$R_{\mathrm{max}}/R_{0}$', 'Interpreter', 'Latex', 'FontSize', 20);
 ylabel('$S/G_1$','Interpreter','Latex','FontSize',24);
@@ -59,4 +60,44 @@ xticks(tickrange)
 tickrange= -5:2:5;
 yticks(tickrange)
 box on;
-saveas(gcf,'./fig_graded_stress_integral','png')
+%saveas(gcf,'./fig_graded_stress_integral','png')
+
+%% figure 2 JMPS
+Pref = 101325;
+p8=1;
+
+tau = @(x) (2/3)*(1./x.^4 - x.^2);
+gtau = @(x,Rst) (G3+(G1-G3)*(1+( (l2*((x.^3 - 1)./(Rst.^3 - 1)).^(1/3) - 1)./...
+        (1-l1*((x.^3 - 1)./(Rst.^3 - 1)).^(1/3)) ).^a).^((n-1)/a));
+
+gtau1 = zeros(size(Rst));
+gtau2 = zeros(size(Rst));
+gtau3 = zeros(size(Rst));
+for i = 1:length(Rst)
+    rst = Rst(i);
+    gtau1(i) = G1*tau(rst);
+    gtau2(i) = gtau(rst,rst)*tau(rst);
+    gtau3(i) = G3*tau(rst);
+end
+
+figure(2)
+hold on;
+plot(Rst,gtau1,'r','LineWidth',3)
+plot(Rst,gtau3,'k--','LineWidth',3)
+plot(Rst,gtau2,'c--','LineWidth',3)
+%plot(Rst,gtau1+gtau2+gtau3,'-.g','LineWidth',3)
+ylim([-5 5])
+xlabel('$R_{\mathrm{max}}/R_{0}$', 'Interpreter', 'Latex', 'FontSize', 20);
+ylabel('$\tau_{rr}/p_{\infty}$','Interpreter','Latex','FontSize',24);
+set(gcf,'color','w');
+set(gca,'FontName','Times','FontSize',20);
+set(gca,'TickLabelInterpreter','latex')
+xa = gca;
+xa.TickLength = [.015 .015];
+xa.LineWidth = 1.5;
+tickrange= 0:2:10;
+xticks(tickrange)
+tickrange= -5:2:5;
+yticks(tickrange)
+box on;
+%saveas(gcf,'./fig_graded_stress_Rst','png')
