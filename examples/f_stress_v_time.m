9478a6e75823ab258bfad7407c3d37d2b87f0210
mcarcana <mirelys_carcana_barbosa@brown.edu>
2025-07-03 13:02:25 -0400

    working stress v time but wrong, needs debugging

--- examples/f_stress_v_time.m
+++ examples/f_stress_v_time.m
@@ -1,47 +1,96 @@
-function [gstress_all, ungstress_all] = f_stress_v_time(R,R0,Req,t,Ca,Ca1,l1,l2,v_nc,v_a)
-nt = length(t);
-lr_N = 500;
-%lr_length = 5;
-%r_coord = ones(lR,lr_N).*logspace(-1,lr_length,lr_N);
-r_coord = linspace(0.1,3, lr_N); %fixed Eulerian grid
-%[taurr1,taurr,~,~] = f_gradedstress(r_coord,R,Req,R0,Ca,Ca1,l1,l2,v_nc,v_a);
+% clc; clear; close all;
 
-nloc=4; %locations to evaluate
+% isgraded = [0 1 0];
+% % positive gradient: soft-to-stiff
+% G0 = [1000 1000 0];
+% G1 = [0 10000 10000];
+% % nondimensionalize
+% Pref=101325;
+% Ca_G0 = Pref./G0;
+% Ca_G1 = Pref./G1;
+% 
+% % additional graded parameters
+% l1 = 1.2;
+% l2 = 2.2;
+% v_a = 2;
+% v_nc = 0.3;
+% 
+% % IMR parameters
+% R0 = 100e-6;
+% Req = R0/8; 
+% tfin = 75E-6;
+% t = linspace(0,tfin,1000);
+% 
+% % parameters
+% nt = length(t);
+% lr_N = nt;
+% lr_length = 3;
+% r_coord = linspace(0,lr_length,lr_N); %fixed Eulerian grid
+% % last location to evaluate stress
+% r_far = max(r_coord)*0.8;
+
+% locations to evaluate
+% nloc=4;
+% location_labels = {sprintf('At R_0',R0),...
+%                    sprintf('Beginning of graded region (l_1 = %.3f)',l1/R0),...
+%                    sprintf('End of graded region (l_2 = %.3f)',l2/R0),...
+%                    sprintf('Far field (r=%.3f)',r_far)};
+% 
+% % looping parameters
+% nmat = length(isgraded); %number of materials
+% stress_all = zeros(length(t), nloc, nmat); %[time, location, material]
+% nt = length(t);
+% 
+% % for each material
+% for i = 1:nmat
+%     Ca = Ca_G0(i);
+%     Ca1 = Ca_G1(i);
+%     stress_all(:,:,i) = f_stress_v_time(nt,nloc,r_coord,R,R0,Req,t,Ca,Ca1,l1,l2,v_nc,v_a);
+%     %stress_all(:,:,i) = stress_all(:,:,i) / -max(max(stress_all(:,:,i)),[],'all');
+% end
+% 
+% % preparing plots
+% labels = {'Soft','Graded','Stiff'};
+% colors = {'r','c--','k--'};
+% 
+% % plotting
+% for loc = 1:nloc
+%     figure
+%     hold on;
+%     for mat = 1:nmat
+%         plot(t,stress_all(:,loc,mat),colors{mat},'LineWidth',3,'DisplayName',labels{mat})
+%     end
+%     xlabel('$t / t_c$', 'Interpreter', 'Latex', 'FontSize', 20);
+%     ylabel('$\tau_{rr} / \max(\tau_{rr})$','Interpreter','Latex','FontSize',20);
+%     title(location_labels{loc},'FontSize',18)
+%     legend show;
+%     hold off;
+% end
+
+function [gstress_all] = f_stress_v_time(isgraded,nt,nloc,r_coord,R,R0,Req,r_far,Ca,Ca1,l1,l2,v_nc,v_a)
+
+% for first two columns of stress_all
 gstress_all = zeros(nt,nloc);
-ungstress_all = zeros(nt,nloc);
 for time_idx = 1:nt
-    %r_spat = r_coord(time_idx,:);
-    %stress_slice = taurr(time_idx,:); unstress_slice = taurr1(time_idx,:);
+    % current time
     Rnow = R(time_idx);
-
-    [taurr1_now,taurr_now,~,~] = f_gradedstress(r_coord,Rnow,Req,R0,Ca,Ca1,l1,l2,v_nc,v_a);
-
-    r1 = ((l1/R0)^3 + Rnow.^3 - Req^3).^(1/3);
-    r2 = ((l2/R0)^3 + Rnow.^3 - Req^3).^(1/3);
-    r_far = max(r_coord)*0.8;
-    r_eval = [R0,r1,r2,r_far];
-
-    for loc = 1:nloc
-        r_current = r_eval(loc);
-        gstress_all(time_idx,loc) = interp1(r_coord,taurr_now,r_current,'pchip');
-        ungstress_all(time_idx,loc) = interp1(r_coord,taurr1_now,r_current,'pchip');
+    
+    % if not graded, compute homogeneous stress
+    if isgraded
+        % get stress profile at current time
+        [taurr_now,r1_now,r2_now] = f_graded_stress(r_coord,Rnow,Req,Ca,Ca1,l1,l2,v_nc,v_a);
+    else
+        [taurr_now,r1_now,r2_now] = f_ng_stress(r_coord,Rnow,Req,Ca,l1,l2);
     end
-end
-location_labels = {sprintf('At R_0',R0),...
-                   sprintf('Beginning of graded region (l_1 = %.3f)',l1/R0),...
-                   sprintf('End of graded region (l_2 = %.3f)',l2/R0),...
-                   sprintf('Far field (r=%.3f)',max(r_coord(:))*0.8)};
-for loc=1:nloc
-    figure
-    hold on;
-    plot(t,gstress_all(:,loc),'r--','LineWidth',3,'DisplayName','graded')
-    plot(t,ungstress_all(:,loc),'b--','LineWidth',3,'DisplayName','ungraded')
-    %formatting plot
-    xlabel('$t / t_c$', 'Interpreter', 'Latex', 'FontSize', 20);
-    ylabel('$\tau_{rr} / \mathrm{max}(\tau_{rr})$','Interpreter','Latex','FontSize',20);
-    %title(sprintf('Stress Comparison at %s',location_labels{loc},'Interpreter','Latex','FontSize',18))
-    title(location_labels{loc},'FontSize',18)
-    legend show;
-    hold off;
+
+    % location of each tracer (at each time)
+    r_eval = [R0,r1_now,r2_now,r_far];
+
+    % interpolate stress at each location of each tracer (at each time)
+    for loc=1:nloc
+        r_current = r_eval(loc);
+        warning('off','MATLAB:interp1:NaNstrip')
+        gstress_all(time_idx,loc) = interp1(r_coord,taurr_now,r_current,'pchip');
+    end
 end
 end
--- s_run_graded.m
+++ s_run_graded.m
@@ -1,19 +1,15 @@
-clc;
-clear;
-close;
+clc; clear; close;
 
+% generate R v t curves
 addpath('src/forward_solver/');
 %addpath('./common/')
 
 % options
-Pref = 101325;
 R0 = 100e-6;
 Req = R0/8; 
 tfin = 75E-6;
-kappa = 1.4;
-T8 = 298.15;
-rho8 = 1064;
 tvector = linspace(0,tfin,1000);
+
 collapse = 0;
 radial = 1;
 vapor = 1;
@@ -21,16 +17,21 @@ bubtherm = 1;
 medtherm = 0;
 masstrans = 0;
 stress = 1;
+
+% graded parameters
 graded = 1;
 v_nc = 0.3; 
 v_a = 2;
 l1 = 1.2*R0; 
 l2 = 2.2*R0;
-G0 = 10E3;
-G1 = 1E3;
+G0 = 1E3;
+G1 = 10E3;
+
+kappa = 1.4;
+T8 = 298.15;
+rho8 = 1064;
 mu = 5E-2;
-Ca = Pref/G0;
-Ca1 = Pref/G1;
+
 varin = {'progdisplay',0,...
     'radial',radial,...
     'bubtherm',bubtherm,...
@@ -57,7 +58,7 @@ varin = {'progdisplay',0,...
     'kappa',kappa,...
     't8',T8,...
     'rho8',rho8};
-
+%% generate R v t data
 [t,R,~] = f_imr_fd(varin{:},'Nt',16,'Mt',64);
 
 figure
@@ -70,150 +71,75 @@ ylim([0 1.2])
 % plot(tg,R,'r--')
 % hold off;
 
-%%
-addpath('src/forward_solver/');
+%% graded and ungraded stress vs time
 
-% equation options
-R0 = 350e-5;
-Req = R0/5;
-tfin = 175E-5;
-kappa = 1;
-Lheat = 2.378193575129533e+04;
-T8 = 298.15;
-rho8 = 1064;
-tvector = linspace(0,tfin,1000);
-radial = 1;
-vapor = 0;%1;
-bubtherm = 0;%1;
-medtherm = 0;%1;
-masstrans = 0;%1;
-stress = 1;
-v_nc = 0.3; 
-v_a = 2;
-l1 = 1.5*R0; 
-l2 = 2.5*R0;
-G0 = [1000];
-G1 = [5000];
-Pref = 101325;
-for i = 1:length(G0)
-    for j = 1:length(G1)
-        Ca = Pref/G0(i);
-        Ca1 = Pref/G1(j);
-        varin = {'progdisplay',0,...
-            'radial',radial,...
-                 'bubtherm',bubtherm,...
-                 'tvector',tvector,...
-                 'vapor',vapor,...
-                 'medtherm',medtherm,...
-                 'method',23,...
-                 'stress',stress,...
-                 'collapse',1,...
-                 'mu',1E0,...%1E-2,...
-                 'g',G0(i),...
-                 'graded',1,...
-                 'g1',G1(j),...
-                 'l1',l1,...
-                 'l2',l2,...
-                 'v_a',v_a,...
-                 'v_nc',v_nc,...
-                 'lambda1',1e-7,...
-                 'lambda2',0,...
-                 'alphax',1e-3,...
-                 'r0',R0,...
-                 'req',Req,...
-                 'kappa',kappa,...
-                 't8',T8,...
-                 'rho8',rho8};
-        
-        [t,R,~] = m_imr_fd(varin{:},'Nt',70,'Mt',70);
-        
-        figure
-        hold on;
-        plot(t,R,'bx')
-        ylim([0 1.2])
+addpath('examples/');
+% G0 = [1000 1000 0];
+% G1 = [0 10000 10000];
+legendName = {'G0 = 1000', 'G0 = 1000, G1 = 10000', 'G1 = 10000'};
 
-        addpath('./examples')
-        f_stress_field
-    end
+isgraded = [0 1 0];
+% positive gradient: soft-to-stiff
+G0 = [1000 1000 10000]; 
+G1 = [0 10000 0];
+% nondimensionalize
+Pref=101325;
+Ca_G0 = Pref./G0;
+Ca_G1 = Pref./G1;
+
+% % parameters
+nt = length(tvector);
+lr_N = nt;
+lr_length = 3;
+r_coord = linspace(0,lr_length,lr_N); %fixed Eulerian grid
+% last location to evaluate stress
+r_far = max(r_coord)*0.8;
+
+% locations to evaluate
+nloc=4;
+location_labels = {sprintf('At R_0',R0),...
+                   sprintf('Beginning of graded region (l_1 = %.3f)',l1/R0),...
+                   sprintf('End of graded region (l_2 = %.3f)',l2/R0),...
+                   sprintf('Far field (r=%.3f)',r_far)};
+
+% looping parameters
+nmat = length(isgraded); %number of materials
+stress_all = zeros(nt, nloc, nmat); %[time, location, material]
+R_all = zeros(nt,nmat);
+
+% for each material
+for i = 1:nmat
+    Ca = Ca_G0(i);
+    Ca1 = Ca_G1(i);
+    % run solver to get R
+    varin = {'tvector',tvector,...
+    'g',G0(i),...
+    'graded',isgraded(i),...
+    'g1',G1(i)};
+    [t,R,~] = f_imr_fd(varin{:},'Nt',16,'Mt',64);
+    % store all R data for each material
+    R_all(:,i) = R;
+    % compute stresses
+    stress_all(:,:,i) = f_stress_v_time(isgraded(i),nt,nloc,r_coord,R,R0,Req,r_far,Ca,Ca1,l1,l2,v_nc,v_a);
+    %stress_all(:,:,i) = stress_all(:,:,i) / -max(max(stress_all(:,:,i)),[],'all');
 end
-%% graded v ungraded 
-clear; close all; clc;
 
-G0 = [1000 1000 5000]; 
-G1 = [0 5000 0];
-graded = [0 1 0];
-legendName = {'G0 = 1000', 'G0 = 1000, G1 = 5000', 'G1 = 5000'};
+% preparing plots
+labels = {'Soft','Graded','Stiff'};
+colors = {'r','c--','k--'};
 
-R0 = 350e-5; Req = R0/5;
-tfin = 175E-5;
-tvector = linspace(0,tfin,1000);
-
-kappa = 1;
-Lheat = 2.378193575129533e+04;
-T8 = 298.15;
-rho8 = 1064;
-
-radial = 1;
-vapor = 1;
-bubtherm = 1;
-medtherm = 1;
-masstrans = 1;
-stress = 1;
-
-v_nc = 0.3; 
-v_a = 2;
-l1 = 1.5*R0; 
-l2 = 5.5*R0;
-Pref = 101325;
-
-for i = 1:length(graded)
-    doing = [graded(i) G0(i) G1(i)];
-    Ca = Pref/G0(i);
-    Ca1 = Pref/G1(i);
-    varin = {'progdisplay',0,...
-        'radial',radial,...
-        'bubtherm',bubtherm,...
-        'tvector',tvector,...
-        'vapor',vapor,...
-        'medtherm',medtherm,...
-        'method',23,...
-        'stress',stress,...
-        'collapse',1,...
-        'mu',1E-2,...
-        'g',G0(i),...
-        'graded',1,...
-        'g1',G1(i),...
-        'l1',l1,...
-        'l2',l2,...
-        'v_a',v_a,...
-        'v_nc',v_nc,...
-        'lambda1',1e-7,...
-        'lambda2',0,...
-        'alphax',1e-3,...
-        'r0',R0,...
-        'req',Req,...
-        'kappa',kappa,...
-        't8',T8,...
-        'rho8',rho8};
-        
-    [t,R,~] = m_imr_fd(varin{:},'Nt',70,'Mt',70);
-    figure(1)
+% plotting
+for loc = 1:nloc
+    figure
     hold on;
-    set(gcf,'color','w');
-    set(gca,'FontName','Times','FontSize',14);
-    set(gca,'TickLabelInterpreter','latex')
-    plot(t,R,'DisplayName',legendName{i})
-    ylim([0 1.2])
+    for mat = 1:nmat
+        plot(t,stress_all(:,loc,mat),colors{mat},'LineWidth',3,'DisplayName',labels{mat})
+    end
+    xlabel('$t / t_c$', 'Interpreter', 'Latex', 'FontSize', 20);
+    ylabel('$\tau_{rr} / \max(\tau_{rr})$','Interpreter','Latex','FontSize',20);
+    title(location_labels{loc},'FontSize',18)
     legend show;
-    xlabel('t/t_c')
-    ylabel('R/R_0')
-
-    addpath('./examples')
-    figure(i+1)
-    clf;
-    f_stress_field
-    % filename = sprintf('./stress_%s.png', legendName{i});
-    % saveas(gcf,filename)
-
+    hold off;
 end
-hold off;
+% filename = sprintf('./stress_%s.png', legendName{i});
+% saveas(gcf,filename)
