7f4081a9f191a834c2a5f0bd7a19f1072578051d
mcarcana <mirelys_carcana_barbosa@brown.edu>
2025-07-04 19:21:42 -0400

    logic in stress_v_time were missing r coordinate logic

--- examples/f_g_stress.m
+++ examples/f_g_stress.m
@@ -19,6 +19,9 @@ r0coord(r0coord < r0shift) = NaN;
 % function for graded region
 f_cy = @(r0coord) (l2 - r0coord) ./ (r0coord - l1);
 
+% initialize taurr
+taurr = zeros(size(r_subset));
+
 % compute stress according to the region
 if r_eval <= l1
     % near field
@@ -28,44 +31,51 @@ if r_eval <= l1
 
 elseif r_eval <= l2
     % near field
-     r0near = r0coord;
-     % zero out regions larger than the near field
-     r0near(r0near > l1) = 0;
-     taurr1 = (2/(3*Ca))*((r0near.^4 ./ r_subset.^4) - (r_subset.^2 ./ r0near.^2));
+     near_mask = r0coord <= l1;
+     r0near = r0coord(near_mask);
+     r_near = r_subset(near_mask);
+     % compute near stress
+     taurr1 = (2/(3*Ca))*((r0near.^4 ./ r_near.^4) - (r_near.^2 ./ r0near.^2));
      % zero out the stress above and below the near field
      taurr1(isinf(taurr1)) = 0;
 
-     % compute the graded region coordinate
-     r0mid = r0coord(r0coord > l1 & r0coord <= l2);
-     % graded stress
+     % graded region
+     mid_mask = r0coord > l1 & r0coord <= l2;
+     r0mid = r0coord(mid_mask);
+     r_mid = r_subset(mid_mask);
+     % compute graded stress
      taurr2 = ((1/Ca) + (1/Ca1 - 1/Ca)*(1+f_cy(r0mid).^v_a).^((v_nc-1)/v_a)).* ...
-            ((r0mid.^4 ./ r_subset.^4) - (r_subset.^2 ./ r0mid.^2));
+            ((r0mid.^4 ./ r_mid.^4) - (r_mid.^2 ./ r0mid.^2));
      % removing the negative infinities
      taurr2(isinf(taurr2)) = 0;
      taurr = taurr1+taurr2;
 
 elseif r_eval > l2
     % near field
-    r0near = r0coord;
-    % zero out regions larger than the near field
-    r0near(r0near > l1) = 0;
-    taurr1 = (2/(3*Ca))*((r0near.^4 ./ r_subset.^4) - (r_subset.^2 ./ r0near.^2));
+    near_mask = r0coord <= l1;
+    r0near = r0coord(near_mask);
+    r_near = r_subset(near_mask);
+    % compute near stress
+    taurr1 = (2/(3*Ca))*((r0near.^4 ./ r_near.^4) - (r_near.^2 ./ r0near.^2));
     % zero out the stress above and below the near field
     taurr1(isinf(taurr1)) = 0;
 
     % compute the graded region coordinate
-    r0mid = r0coord(r0coord > l1 & r0coord < l2);
-    % graded stress
-    taurr2 = ((1/Ca) + (1/Ca1 - 1/Ca)*(1+f_cy.^v_a).^((v_nc-1)/v_a)).* ...
-        ((r0mid.^4 ./ r_subset.^4) - (r_subset.^2 ./ r0mid.^2));
+    mid_mask = r0coord > l1 & r0coord <= l2;
+    r0mid = r0coord(mid_mask);
+    r_mid = r_subset(mid_mask);
+    % compute graded stress
+    taurr2 = ((1/Ca) + (1/Ca1 - 1/Ca)*(1+f_cy(r0mid).^v_a).^((v_nc-1)/v_a)).* ...
+        ((r0mid.^4 ./ r_mid.^4) - (r_mid.^2 ./ r0mid.^2));
     % removing the negative infinities
     taurr2(isinf(taurr2)) = 0;
 
     % far field
-    r0far = r0coord > l2;
-    % zero out regions less than the far field
-    r0far(r0far < l2) = 0;
-    taurr3 = (2/(3*Ca1))*((r0far.^4 ./ r_subset.^4) - (r_subset.^2 ./ r0far.^2));
+    far_mask = r0coord > l2;
+    r0far = r0coord(far_mask);
+    r_far = r_subset(far_mask);
+    % compute far stress
+    taurr3 = (2/(3*Ca1))*((r0far.^4 ./ r_far.^4) - (r_far.^2 ./ r0far.^2));
     % zero out the stress below the far field
     taurr3(isinf(taurr3)) = 0;
     taurr = taurr1 + taurr2 + taurr3;
