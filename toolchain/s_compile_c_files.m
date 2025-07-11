aaa03cdc5ef9b01d90f49dd75c02dece5648eff4
mcarcana <mirelys_carcana_barbosa@brown.edu>
2025-07-11 10:46:54 -0400

    adding github workflow action for compiling c files for all project

---
+++ toolchain/s_compile_c_files.m
@@ -0,0 +1,18 @@
+% file s_compile_c_files.m
+% brief contains script to recursively find and compile all .c files in project
+clc;
+close all;
+clear;
+
+rootDir = fileparts(pwd);
+% recursively search through project for any c files
+cfiles = dir(fullfile(rootDir, '**', '*.c'));
+for k = 1:numel(cfiles)
+	mexPath = fullfile(cfiles(k).folder, cfiles(k).name);
+	try
+		mex(mexPath);
+		fprintf('Compiled: %s\n', mexPath);
+	catch ME
+		warning('Failed to compile %s: %s\n', mexPath, ME.message);
+	end
+end
