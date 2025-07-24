89bd5a9cee0366e105081b2e0721008fb2cd8fe6
mcarcana <mirelys_carcana_barbosa@brown.edu>
2025-07-24 12:26:55 -0400

    debugging why collapse time imaginary

--- src/characterization/f_tcol_calc_graded.m
+++ src/characterization/f_tcol_calc_graded.m
@@ -6,8 +6,8 @@
 % elasticity.
 function [tg] = f_tcol_calc_graded(stress,Req,R,R0,Ca,Ca1,Pref,l1,l2,v_a,v_nc,rho8)
     dtg_vals = zeros(1,length(R));
-    el1 = l1/R0;
-    el2 = l2/R0; %I think these both should be ()/Req
+    Eem_vals = zeros(1,length(R));
+    Ee_vals = zeros(1,length(R));
     for i = 1:length(R)
         Rnow = R(i);
         % radial stretch
@@ -16,12 +16,12 @@ function [tg] = f_tcol_calc_graded(stress,Req,R,R0,Ca,Ca1,Pref,l1,l2,v_a,v_nc,rh
         Rst = 1./Rs; %Lambda
         Rmt = R0/Req; %Lambda_m
         
-        x1 = 1 + ((Rst.^3 -1)./((1+el1)^3)).^(1/3); %Lambda_1
-        x2 = 1 + ((Rst.^3 -1)./((1+el2)^3)).^(1/3); %Lambda_2
-        xm1 = 1 + ((Rmt^3 -1)/((1+el1)^3))^(1/3); %Lambda_m1
-        xm2 = 1 + ((Rmt^3 -1)/((1+el2)^3))^(1/3); %Lambda_m2
-        
-        f_cy = @(x) ( el2.*((x.^3 - 1)./(Rst.^3 - 1)).^(1/3) - 1 ) ./ ( 1-el1.*((x.^3 - 1)./(Rst.^3 - 1)).^(1/3) );
+        x1 = 1 + ((Rst.^3 -1)./(l1^3)).^(1/3); %Lambda_1
+        x2 = 1 + ((Rst.^3 -1)./(l2)^3).^(1/3); %Lambda_2
+        xm1 = 1 + ((Rmt^3 -1)/(l1)^3)^(1/3); %Lambda_m1
+        xm2 = 1 + ((Rmt^3 -1)/(l2)^3)^(1/3); %Lambda_m2
+
+        f_cy = @(x) ( l2.*(((x.^3 - 1)./(Rst.^3 - 1)).^(1/3)) - 1 ) ./ ( 1 - l1.*((x.^3 - 1)./(Rst.^3 - 1)).^(1/3));
         m = @(x) (1 + f_cy(x).^v_a).^((v_nc-1)/v_a);
         ee2integ = @(x) (1/Ca + (1/Ca1 - 1/Ca).* m(x)) .* ((1./x.^2) + 2.*x.^4 - 3.*x.^2)./((x.^3 - 1).^2);
         
@@ -37,6 +37,9 @@ function [tg] = f_tcol_calc_graded(stress,Req,R,R0,Ca,Ca1,Pref,l1,l2,v_a,v_nc,rh
             Eem3 = (1/Ca1)*((5/3)- (1/xm2) -((1+xm2)/(1+ xm2 +xm2^2)));
             Ee = Ee1 + Ee2 + Ee3;
             Eem = Eem1 + Eem2 + Eem3;
+
+            Eem_vals(i) = Eem;
+            Ee_vals(i) = Ee;
             
             %need rho in 1/Ca?
             %dtg = @(R) -1./sqrt( (2/3)*(Pref/rho)*(Rm.^3 -1) + 2*(Rm.^3 - Rs.^3).*Eem - 2*(1 - Rs.^3).*Ee );
@@ -51,8 +54,16 @@ function [tg] = f_tcol_calc_graded(stress,Req,R,R0,Ca,Ca1,Pref,l1,l2,v_a,v_nc,rh
             
             % dtg_vals(i) = -1/sqrt(abs(dtg_sq));
             dtg_vals(i) = -1/sqrt(dtg_sq);
+            fprintf('i=%d, R =%.5e, dtg_sq=%.5e\n', i , Rnow, dtg_sq);
+            if dtg_sq < 0
+                warning('Imaginary collapse time at R = %.3e', Rnow);
+            end
         end
     end
+    figure
+    hold on
+    plot(R, Eem_vals, 'k')
+    plot(R,Ee_vals,'b--')
     % dtg_sq;
     % dtg_vals
     tg = trapz(R,dtg_vals);
