b1faf98d7d940d60ed9d5394fa7af398404c3d39
mcarcana <mirelys_carcana_barbosa@brown.edu>
2025-07-24 12:25:57 -0400

    added Gaussian pulse waveform in src code, expression from Hersey2023/Preston2007

--- src/forward_solver/f_pinfinity.m
+++ src/forward_solver/f_pinfinity.m
@@ -61,5 +61,11 @@ function [p8,p8dot] = f_pinfinity(t,vararg)
         p = -ee*(1-heaviside(t-tw));
         pdot = 0;
     end
+
+    % gaussian waveform
+    function [p,pdot] = gaussian(t)
+        p = 1 - ee * exp(-((t-dt)^2) / tw^2);
+        pdot = ee * (2*(t-dt)/tw^2) * exp(-((t-dt)^2) / tw^2);
+    end
     
 end
