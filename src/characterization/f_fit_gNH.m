4249a2eab135d4559cb82ace8962f2413fcf26ce
mcarcana <mirelys_carcana_barbosa@brown.edu>
2025-08-08 10:42:37 -0400

    barebones characterization scheme for graded NH model based on pIMR

---
+++ src/characterization/f_fit_gNH.m
@@ -0,0 +1,73 @@
+% file f_f_gNH.m
+% brief contains function f_fit_gNH
+
+% brief This function estimates graded collapse time for a given set of graded input data
+% describing a Neo-Hookean Newtonian material ongoing inertial cavitation.
+function t1approx_vals = f_fit_gNH(G0_guess,G1_guess,l1_guess,l2_guess,va_guess,vnc_guess,data_fit)
+
+% Inputs:
+%   G_guess - base elastic shear modulus (Pa)
+%   G1_guess - upper elastic shear modulus (Pa)
+%   l1_guess - l1 wrt R0 (m)
+%   l2_guess - l1 wrt R0 (m)
+%   va_guess - Carreau-Yasuda parameter
+%   vnc_guess - Carreau-Yasuda parameter
+
+% Unwrap input
+nX = length(data_fit(:,1));
+RX = data_fit(:,1);
+LX = data_fit(:,2);
+f_Ma = data_fit(:,3);
+f_We = data_fit(:,4);
+f_gas = data_fit(:,5);
+Ca_scale = data_fit(:,6);
+
+% Other constants to use:
+trc = sqrt(pi/6)*gamma(5/6)/gamma(4/3); % ~= 0.9147
+
+% fitting parameters (intended to be the same value for all experiments)
+Ca_guess = Ca_scale(1)/G0_guess(1);
+Ca1_guess = Ca_scale(1)/G1_guess(1); 
+
+reltol = 1e-1; abstol = 1e-1;
+t1approx_vals = zeros(1:nX);
+for i = 1:nX
+    Req_i = RX(i)./LX(i);
+    el1 = l1_guess ./ Req_i;
+    el2 = l2_guess ./ Req_i;
+    % let x be R
+    Rst = @(x) x./Req_i;
+    x1 = @(x) (1 + (Rst(x).^3 - 1)./(el1.^3)).^(1/3);
+    x2 = @(x) (1 + (Rst(x).^3 - 1)./(el2.^3)).^(1/3);
+
+    % let y be lambda
+    fnum_cy = @(y,x) el2.*((y.^3 - 1)./(Rst(x).^3 - 1)).^(1/3) - 1;
+    fden_cy = @(y,x) 1-el1.*((y.^3 - 1)./(Rst(x).^3 - 1)).^(1/3);
+    f_cy = @(y,x) fnum_cy(y,x)./fden_cy(y,x);
+    g_handle = @(y,x) (1/Ca_guess + (1/Ca1_guess - 1/Ca_guess).*(1+f_cy(y,x).^va_guess).^((vnc_guess-1)/va_guess)).*...
+                ((1./y.^5) + (1./y.^2));
+
+    %neo-Hookean elasticity
+    Se1 = @(x) (1/(2*Ca_guess)).*(Req_i.^4 ./ x.^4  +  4.*Req_i ./ x  -  1./x1(x).^4  -  4./x1(x));
+    Se2 = @(x) arrayfun(@(xi) 2*integral(@(y) g_handle(y,xi),x1(xi),x2(xi),'RelTol',reltol,'AbsTol',abstol),x);
+    Se3 = @(x) -(1/(2*Ca1_guess)).*(5 - 4./x2(x) - 1./x2(x).^4);
+    Se = @(x) Se1(x) + Se2(x) + Se3(x);
+
+    % compute \bar{f}_gradedelastic^*
+    feg_int = @(x) -1./((2/3)*(1./x.^3 - 1)).^0.5; % SINGULARITY AT X=1
+    eps = 1e-7;
+    f_eg = -1/trc * integral(@(x) Se(x).*feg_int(x),eps,1-eps,'RelTol',reltol,'AbsTol',abstol);
+    % f_eg = 1/trc * integral(@(x) Se(x).*feg_int(x),1,0,'RelTol',reltol,'AbsTol',abstol);
+
+    fsum = 1 - f_eg - f_We(i) - f_Ma(i) - f_gas(i);
+    % find approximate collapse time
+    if min(fsum) < 0
+        t1_approx = nan;
+    else
+        t1_approx = (fsum).^(-1/2);
+    end
+
+    % store values
+    t1approx_vals(i) = t1_approx;
+end
+end 
---
+++ src/characterization/main_gpIMR.m
@@ -0,0 +1,135 @@
+% Parsimonious IMR (pIMR) toward rapid first-estimate of viscoelastic
+% properties: this main script reads a datafile containing micro-cavitation
+% experiments and fits for a suitable set of viscoelastic properties
+
+% acquiring required data input
+infile = 'data.mat';   % Name of file to read
+load(infile);
+
+infile2 = 'Rdata.mat'; % WHY DOES THIS RESULT IN 1X1 CELL, THOUGH IN IMR/SRC/CHARACT IT'S NX1??
+load(infile2);
+
+% reading required data input
+nX = size(data,1);   % # of experiments
+RX = data(:,1);      % All Rmax 
+LX = data(:,2);      % All amplification Lmax
+T1X = data(:,3);     % All collapse time t1
+
+% physical parameters 
+% Atmospheric Pressure (Pa)
+p_inf = 101325;
+% Density of characterized material (kg/m^3)
+rho = 998.2;
+% Surface tension (N/m)
+gam = 0.070;
+% Wave speed in characterized material (m/s) 
+cwave = 1484;
+% Saturated vapor pressure at far-field temperature (Pa) 
+pvsat = 3116.7757;
+% pIMR non-viscoelastic parameters
+C_kap = 1.4942;
+
+% setting up model parameters
+pbar = p_inf;
+We = pbar*RX/(2*gam);
+uc = sqrt(pbar/rho);
+Ma = cwave/uc;
+% ARC ~= 1/0.9147 = 1/trc
+ARC = 1/( sqrt(pi/6)*gamma(5/6)/gamma(4/3) );
+f_Ma = (2/sqrt( 1 + (1 + 4*(Ma/ARC)^2 ))) * ones(nX,1);
+f_We = - pi*ARC./(sqrt(6) * We); % This varies with Rmax
+Req = (RX./LX);
+PG0 = (p_inf).*(LX.^(-3));  
+Alpha = 1 + pvsat./PG0;
+f_gas = C_kap*(LX.^(-3)).*Alpha;
+Ca_scale = pbar*ones(nX,1); % Ca*G
+Re_scale = rho*uc*RX; % = Re*mu
+De_scale = RX/uc; % characteristic time scale
+
+% Construct new matrix to pass to solver:
+data_fit = [RX,LX,f_Ma,f_We,f_gas,Ca_scale,Re_scale,De_scale];
+
+% Convert T1X to dimensionless, relative to t_{RC} at each scale.
+tRC = (RX/uc)/ARC;
+T1_ND = T1X./tRC;
+
+% graded NH fit
+t1approx_vals = @(X) f_fit_gNH(X(1),X(2),X(3),X(4),X(5),X(6),data_fit);
+err_gNH = @(X) (T1_ND./t1approx_vals(X)).^2 - 1;
+err_fn_gNH = @(X) log10((err_gNH(X))'*(err_gNH(X))/nX);
+
+% define search range and initial start for each graded parameter (dim)
+G0_min = 1E-3; 
+G0_max = 1E5; % max 1E6 
+G0_start = 1E3;
+
+G1_min = 1E-3;
+G1_max = 1E5; % max 1E6
+G1_start = 10E3;
+
+% l1_min is at least 1*max(Req)
+Req_max = max(RX)/min(LX);
+l1_min = Req_max;
+l1_start = 1.2E-4; %2*Req_max;
+l1_max = l1_start + 1e-6; %8*Req_max;
+
+% l2 must be greater than l1
+l2_min = l1_start + 1e-6; 
+l2_max = 5*Req_max; %10*Req_max; 
+l2_start = 1.8e-4; %3*Req_max;
+
+% min should be greater than 1, upperbound?
+va_min = 1; 
+va_max = 4;
+va_start = 2;
+
+% should be less than 0.5, lowerbound?
+vnc_min = 0;
+vnc_max = 0.5;
+vnc_start = 0.3;
+
+%%
+% contains all X values the optimizer tries
+X_log = [];
+% add options to set optimization tolerances
+opts = optimset('Display','iter','TolX',1e-1,'TolFun',1e-1,'MaxIter',50,'MaxFunEvals',120,'OutputFcn', @output_log);
+
+% optimizing based on search ranges
+opt_fit_gNH = fminsearchbnd(err_fn_gNH,...
+                [G0_start,G1_start,l1_start,l2_start,va_start,vnc_start],...
+                [G0_min,G1_min,l1_min,l2_min,va_min,vnc_min],...
+                [G0_max,G1_max,l1_max,l2_max,va_max,vnc_max],...
+                opts);
+% extract best fit of each parameter
+G0_opt_NH = opt_fit_gNH(1);
+G1_opt_NH = opt_fit_gNH(2);
+l1_opt_NH = opt_fit_gNH(3);
+l2_opt_NH = opt_fit_gNH(4);
+va_opt_NH = opt_fit_gNH(5);
+vnc_opt_NH = opt_fit_gNH(6);
+disp("Overall Best Fit: G0 = " + G0_opt_NH + " Pa, G1 = " + G1_opt_NH + " Pa, l1 = " + l1_opt_NH + "m, l2 = " + l2_opt_NH + "m, v_a = " + va_opt_NH + ", v_nc = " + vnc_opt_NH)
+%%
+% reconstruct all t1approx_vals
+t1_approx_all = zeros(size(X_log,1),length(T1_ND));
+for i = 1:size(X_log)
+    X = X_log(i,:);
+    err_vec = (T1_ND ./ fit_gNH(X(1),X(2),X(3),X(4),X(5),X(6),data_fit)).^2 - 1;
+    format long;
+    t1_approx_all(i, :) = T1_ND ./ sqrt(err_vec + 1);  % exact inverse
+end
+%%
+% Also find single-parameter fits:
+opt_fit_NHG0 = fminsearchbnd(err_fn_gNH,[G0_start,0,0,0,0,0],[G0_min,0,0,0,0,0],[G0_max,0,0,0,0,0]);
+opt_fit_NHG1 = fminsearchbnd(err_fn_gNH,[0,G1_start,0,0,0,0],[0,G1_min,0,0,0,0],[0,G1_max,0,0,0,0]);
+opt_fit_NHl1 = fminsearchbnd(err_fn_gNH,[0,0,l1_start,0,0,0],[0,0,l1_min,0,0,0],[0,0,l1_max,0,0,0]);
+opt_fit_NHl2 = fminsearchbnd(err_fn_gNH,[0,0,0,l2_start,0,0],[0,0,0,l2_min,0,0],[0,0,0,l2_max,0,0]);
+opt_fit_NHva = fminsearchbnd(err_fn_gNH,[0,0,0,0,va_start,0],[0,0,0,0,va_min,0],[0,0,0,0,va_max,0]);
+opt_fit_NHvnc = fminsearchbnd(err_fn_gNH,[0,0,0,0,0,vnc_start],[0,0,0,0,0,vnc_min],[0,0,0,0,0,vnc_max]);
+
+disp("NH Best Fit for near field: G = " + opt_fit_NHG0(1) + " Pa.")
+disp("NH Best Fit for far field: G1 = " + opt_fit_NHG1(1) + " Pa.")
+disp("NH Best Fit for near field region: l1 = " + opt_fit_NHl1(1) + "m")
+disp("NH Best Fit for far field region: l2 = " + opt_fit_NHl2(1) + "m")
+disp("Best Fit for C-Y parameter: v_a = " + opt_fit_NHva(1))
+disp("Best Fit for C-Y parameter: v_nc = " + opt_fit_NHvnc(1))
+disp("Overall Best Fit: G0 = " + G0_opt_NH + " Pa, G1 = " + G1_opt_NH + " Pa, l1 = " + l1_opt_NH + "m, l2 = " + l2_opt_NH + "m, v_a = " + va_opt_NH + ", v_nc = " + vnc_opt_NH)
